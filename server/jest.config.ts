f68eb91d0f140859790cd3585f414268a84865eb
zivserphos <zivfromisrael@gmail.com>
2022-01-21 14:07:47 +0200

    deleted unnecessary server folder
    server folder came with the template I built for this kind of projects, it is not necessary here

--- server/jest.config.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from "@jest/types";
-
-const config: Config.InitialOptions = {
-  testEnvironment: "node",
-  roots: ["<rootDir>/tests"],
-  preset: "ts-jest",
-};
-export default config;
