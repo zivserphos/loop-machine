07447319cfa0acea37014a47c2aabce8e891f3e6
zivserphos <zivfromisrael@gmail.com>
2022-01-21 14:55:46 +0200

    changed channel chunks test style

--- client/src/components/Channel-Chunk.tsx
+++ client/src/components/Channel-Chunk.tsx
@@ -3,15 +3,8 @@ import { Col } from "react-bootstrap";
 
 const ChannelChunk = function ({ index }: { index: number }) {
   return (
-    <Col
-      xs={1}
-      sm={1}
-      md={1}
-      lg={1}
-      className="channel-chunk"
-      style={{ borderTop: "solid blue 7px" }}
-    >
-      {`${index + 1} of 8`}
+    <Col xs={1} sm={1} md={1} lg={1} className="channel-chunk" style={{}}>
+      {`0${index}:00-0${index + 1}:00`}
     </Col>
   );
 };
--- client/src/state/song/helpers/helpers.ts
+++ client/src/state/song/helpers/helpers.ts
@@ -8,12 +8,12 @@ const updateSong = (song: Song, state: SongState): SongState => {
 };
 
 const updateTime = (seconds: number, state: SongState): SongState => {
-  const updatedCursor = 51.3 + (5 / 24) * seconds;
+  const updatedCursor = 51.7 + (5 / 24) * seconds;
   return { ...state, seconds, cursorMarginLeft: updatedCursor };
 };
 
 const updateCursor = (updatedCursor: number, state: SongState): SongState => {
-  const seconds = (updatedCursor - 51.3) / (5 / 24);
+  const seconds = (updatedCursor - 51.7) / (5 / 24);
   return { ...state, seconds, cursorMarginLeft: updatedCursor };
 };
 
